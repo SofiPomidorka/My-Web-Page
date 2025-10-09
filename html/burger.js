68f8961e61dfa01835180161bdcc1d8a613c49db
SofiPomidorka <maximovasofya0@gmail.com>
2025-10-09 20:43:03 +0300

    burger for all

--- html/burger.js
+++ html/burger.js
@@ -2,3 +2,15 @@ document.querySelector('.burger').addEventListener('click', function(){
     this.classList.toggle('active');
     document.querySelector('.nav').classList.toggle('open');
     })
+    const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('active');
+
+  if (nav.classList.contains('active')) {
+    document.body.style.overflow = 'hidden';  
+  } else {
+    document.body.style.overflow = '';        
+  }
+});
